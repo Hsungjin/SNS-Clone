760a7f09a51c4c2570be3a659cfc5e527db7c496
Hsungjin <hsungjin142@gmail.com>
2024-01-10 02:24:53 +0900

    [Design] Profile View UI 추가

---
+++ Thread/Thread/Core/Profile/Model/ProfileThreadFilter.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileThreadFilter.swift
+//  Thread
+//
+//  Created by 황성진 on 1/10/24.
+//
+
+import Foundation
+
+enum ProfileThreadFilter: Int, CaseIterable, Identifiable {
+    case threads
+    case replies
+    
+    var title: String {
+        switch self {
+        case .threads: return "Threads"
+        case .replies: return "Replies"
+        }
+    }
+    
+    var id: Int { return self.rawValue }
+    
+}
--- Thread/Thread/Core/Profile/View/ProfileView.swift
+++ Thread/Thread/Core/Profile/View/ProfileView.swift
@@ -8,8 +8,95 @@
 import SwiftUI
 
 struct ProfileView: View {
+    // MARK: - PROPERTY
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var filterBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 16
+    }
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Profile")
+        ScrollView(showsIndicators: false) {
+            // bio and state
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        // fullname and username
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("길동이")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("길동이")
+                                .font(.subheadline)
+                            
+                        } //: VSTACK
+                        
+                        Text("냐아아옹")
+                            .font(.footnote)
+                        
+                        Text("2 followers")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    } //: VSTACK
+                    Spacer()
+                    
+                    CircularProfileImageView()
+                } //: HSTACK
+                
+                Button {
+                    
+                } label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                // user content list view
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundStyle(.black)
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "itme", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .foregroundStyle(.clear)
+                                        .frame(width: filterBarWidth, height: 1)
+                                }
+                            } //: VSTACK
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    selectedFilter = filter
+                                }
+                            }
+                        } //: LOOP
+                    } //: HSTACK
+                    LazyVStack {
+                        ForEach(0 ... 10, id: \.self) { thread in
+                            ThreadCell()
+                        }
+                    } //: LAZYVSTACK
+                    .padding(.vertical, 8)
+                } //: VSTACK
+
+            } //: VSTACK
+        } //: SCROLL
+        .padding(.horizontal)
     }
 }
 
