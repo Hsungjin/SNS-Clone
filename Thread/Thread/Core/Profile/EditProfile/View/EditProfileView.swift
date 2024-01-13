b63b3d7d62251db00847811efdd254b5b3d55f99
Hsungjin <hsungjin142@gmail.com>
2024-01-14 08:11:43 +0900

    [Feat] Profile이미지 Storage 저장하기

--- Thread/Thread/Core/Profile/EditProfile/View/EditProfileView.swift
+++ Thread/Thread/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -14,7 +14,7 @@ struct EditProfileView: View {
     @State private var link = ""
     @State private var isPrivateProfile = false
     @Environment(\.dismiss) var dismiss
-    @EnvironmentObject var viewModel: CurrentUserProfileViewModel
+    @StateObject var viewModel = EditProfileViewModel()
     
     // MARK: - BODY
     var body: some View {
@@ -96,7 +96,10 @@ struct EditProfileView: View {
                 
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Done") {
-                        
+                        Task {
+                            try await viewModel.updateUserData()
+                            dismiss()
+                        }
                     }
                     .font(.subheadline)
                     .fontWeight(.semibold)
